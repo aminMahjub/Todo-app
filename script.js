a6add7d78b5ad2517679e1f3175adbddebf70e54
aminMahjub <aminmahjoob3172@gmail.com>
2021-04-07 02:54:01 +0430

    make responsive to-do site

--- script.js
+++ script.js
@@ -14,4 +14,20 @@ function TodoFunction()
     ul.appendChild(li);
     }
         document.getElementsByClassName('input')[0].value = " ";
+
+    
+    var deleteButton = document.createElement("button");
+    deleteButton.appendChild(document.createTextNode("Delete"));
+    deleteButton.className += "delete-btn";
+    li.appendChild(deleteButton);
+
+    var editButton = document.createElement("button");
+    editButton.appendChild(document.createTextNode("Edit"));
+    editButton.className += "edit-btn";
+    li.appendChild(editButton);
+
+    var completeButton = document.createElement("button");
+    completeButton.appendChild(document.createTextNode("Complete"));
+    completeButton.className += "complete-btn";
+    li.appendChild(completeButton);
 }
