7c383cf2778af787969e48573bb0413f55892fe6
aminMahjub <aminmahjoob3172@gmail.com>
2021-04-11 00:02:53 +0430

    filter bug

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+var li = document.createElement("li");
+var toggleClass = li.classList.toggle("completed-item");
 function TodoFunction() {
     var text = document.getElementsByClassName('input')[0].value;
     if (text == "" || isNaN(text) !== true) {
         alert("Input is not unacceptable");
     } else {
-
         var li = document.createElement("li");
         var txt = document.createTextNode(text);
         li.appendChild(txt);
@@ -28,20 +29,20 @@ function TodoFunction() {
         li.style.display = "none";
     })
     var completeButtonCls = document.getElementsByClassName("complete-btn")[0];
-    completeButton.addEventListener("click", function() {
+    completeButton.addEventListener("click", function completeItem() {
         var liStyle = li.style;
         var completeBtnStyle = completeButton.style;
         if (li.style.backgroundColor == "green") {
             li.style = "";
             completeButton.style = ""
-            completeButton.innerHTML = "Complete"
+            completeButton.innerHTML = "Complete";
         } else {
             liStyle.backgroundColor = "green";
             liStyle.color = "white"
             completeBtnStyle.backgroundColor = "grey";
             completeButton.innerHTML = "&#10004;"
-
         }
+        li.classList.toggle("completed-item");
     });
     var editButtonCls = document.getElementsByClassName("edit-btn")[0];
     li.setAttribute("contentEditable", "false");
@@ -59,6 +60,30 @@ function TodoFunction() {
                 buttonGroup[i].contentEditable = false;
             }
         }
+    });
 
-    })
+}
+function filterTask() {
+    var selectElement = document.getElementsByClassName("filter")[0];
+    var i = selectElement.selectedIndex;
+    if(i == 0){
+alert(1)
+    } else if(i == 1) {
+        debugger;
+        var parentLi = document.getElementById("demo");
+        var uncompletedItem = document.getElementsByClassName("completed-item");
+        var k;
+        var y = uncompletedItem.length - 1;
+        for(k = 0; k < y ; k++ ) {
+            parentLi.removeChild(uncompletedItem[k]);
+        }
+
+alert(2)
+
+    } else {
+        
+alert(3)
+
+    }
+    
 }
