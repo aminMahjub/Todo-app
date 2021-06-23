17361df9a15c77db58fa397659fd2bafb341ca78
aminMahjub <aminmahjoob3172@gmail.com>
2021-06-23 15:07:43 +0430

    last commit

--- script.js
+++ script.js
@@ -1,148 +1,146 @@
-var Task, TodoList, deleteButton, editButton, completeButton;
+let taskDataBase = [];
+const TodoList = document.querySelector(".todo-list");
 
-  function addTask() {
-    var inputValue = document.getElementsByTagName("input")[0];
-    TodoList = document.getElementsByClassName("todo-list")[0];
-    Task = document.createElement("li");
-    var txt = document.createTextNode(inputValue.value);
-    if (inputValue.value == "" || isNaN(inputValue.value) !== true) {
-      alert("Input is not unacceptable");
-    } else {
-      Task.classList.add("li-item");
-      Task.appendChild(txt);
-      TodoList.appendChild(Task);
+const inputValue = document.querySelector("input");
+let status = "all";
+let countId = 0;
+
+function addTask() {
+  if (inputValue.value == "") {
+    alert("hhhh");
+    return;
+  } else {
+    dataBase(inputValue.value);
+    renderPage();
+    inputValue.value = "";
+  }
+}
+
+function dataBase(text) {
+  let taskObj = {
+    text: text,
+    completeStatus: false,
+    id: countId,
+    edit: false,
+  };
+  countId++;
+  taskDataBase.push(taskObj);
+}
+
+function renderPage() {
+  TodoList.innerHTML = "";
+  const filteredDataBase = taskDataBase.filter((task) => {
+    switch (status) {
+      case "all": {
+        return true;
+      }
+      case "completed": {
+        if (task.completeStatus) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+      case "active": {
+        if (!task.completeStatus) {
+          return true;
+        } else {
+          return false;
+        }
+      }
     }
-    inputValue.value = " ";
-    createButton();
-    deleteButton.addEventListener("click", deleteTask);
+  });
+  filteredDataBase.forEach(function (taskArry) {
+    const { id, completeStatus, text , edit} = taskArry;
+    const Task = document.createElement("li");
+    Task.classList.add("li-item");
+    Task.textContent = text;
+    Task.id = `${id}`;
+    if (completeStatus) {
+      Task.classList.remove("complete-btn");
+      Task.classList.add("complete-status");
+    }
+    if (edit) {
+      Task.contentEditable = true;
+    }
+    createButton(Task);
+    TodoList.appendChild(Task);
+  });
+}
+
+function createButton(Task) {
+  const deleteButton = document.createElement("button");
+  const editButton = document.createElement("button");
+  const completeButton = document.createElement("button");
+
+  deleteButton.appendChild(document.createTextNode("Delete"));
+  deleteButton.classList.add("delete-btn");
+  Task.appendChild(deleteButton);
+  deleteButton.addEventListener("click", (event) => {
+    deleteTask(event);
+  });
+
+  editButton.appendChild(document.createTextNode("Edit"));
+  editButton.classList.add("edit-btn");
+  Task.appendChild(editButton);
+  editButton.addEventListener("click", (event) => {
+    editTask(event);
+  });
+
+  completeButton.appendChild(document.createTextNode("Complete"));
+  completeButton.classList.add("complete-btn");
+  completeButton.addEventListener("click", (event) => {
+    completeTask(event);
+  });
+  Task.appendChild(completeButton);
+}
+
+function editTask(event) {
+  event.preventDefault();
+  const editButton = event.target;
+  const li = editButton.parentElement;
+  const id = +li.id;
+
+  for (let task of taskDataBase) {
+    if (task.id === id) {
+      task.edit = !task.edit;
+      task.text = li.childNodes[0].nodeValue;
+      break;
+    }
   }
+  renderPage();
+}
 
-  function createButton() {
-    deleteButton = document.createElement("button");
-    editButton = document.createElement("button");
-    completeButton = document.createElement("button");
-    deleteButton.appendChild(document.createTextNode("Delete"));
-    deleteButton.classList.add("delete-btn");
-    Task.appendChild(deleteButton);
-    editButton.appendChild(document.createTextNode("Edit"));
-    Task.setAttribute("contentEditable", "false");
-    editButton.classList.add("edit-btn");
-    Task.appendChild(editButton);
-    completeButton.appendChild(document.createTextNode("Complete"));
-    completeButton.classList.add("complete-btn");
-    Task.appendChild(completeButton);
+function deleteTask(event) {
+  event.preventDefault();
+  const deleteButton = event.target;
+  const li = deleteButton.parentElement;
+  const id = +li.id;
+
+  taskDataBase = taskDataBase.filter((task) => {
+    return task.id !== id;
+  });
+  renderPage();
+}
+
+function completeTask(event) {
+  event.preventDefault();
+  const completeButton = event.target;
+  const li = completeButton.parentElement;
+  const id = +li.id;
+
+  for (let task of taskDataBase) {
+    if (task.id === id) {
+      task.completeStatus = !task.completeStatus;
+      break;
+    }
   }
+  renderPage();
+}
+function filterTask() {
+  const filterElement = document.getElementsByClassName("filter")[0];
+  const filterELChild = filterElement.value.toLowerCase();
 
-  function deleteTask() {
-    TodoList.removeChild(Task);
-  }
-
-// function createButton() {
-//   debugger;
-//   deleteButton = document.createElement("button");
-//   editButton = document.createElement("button");
-//   completeButton = document.createElement("button");
-//   deleteButton.appendChild(document.createTextNode("Delete"));
-//   deleteButton.classList.add("delete-btn");
-//   Task.appendChild(deleteButton);
-//   editButton.appendChild(document.createTextNode("Edit"));
-//   Task.setAttribute("contentEditable", "false");
-//   editButton.classList.add("edit-btn");
-//   Task.appendChild(editButton);
-//   completeButton.appendChild(document.createTextNode("Complete"));
-//   completeButton.classList.add("complete-btn");
-//   Task.appendChild(completeButton);
-// }
-// function deleteTask() {
-//     TodoList.removeChild(Task);
-
-// }
-// function createButton() {
-//     debugger;
-//     var deleteButton, editButton, completeButton;
-//     deleteButton = document.createElement("button");
-//     editButton = document.createElement("button");
-//     completeButton = document.createElement("button");
-//     deleteButton.appendChild(document.createTextNode("Delete"));
-//     deleteButton.classList.add("delete-btn");
-//     Task.appendChild(deleteButton);
-//     editButton.appendChild(document.createTextNode("Edit"));
-//     Task.setAttribute("contentEditable", "false");
-//     editButton.classList.add("edit-btn");
-//     Task.appendChild(editButton);
-//     completeButton.appendChild(document.createTextNode("Complete"));
-//     completeButton.classList.add("complete-btn");
-//     Task.appendChild(completeButton);
-//     }
-// function filterTask() {
-//     var filterElement, filterChild, completedTask, unCompletedTask, completedTaskArr, unCompleteTaskArr, k, j;
-//     filterElement = document.getElementsByClassName("filter")[0];
-//     filterChild = filterElement.selectedIndex;
-//     completedTask = document.querySelectorAll(".completed-item");
-//     unCompletedTask = document.querySelectorAll('.li-item');
-
-//     function completedItem(x) {
-//         for (k = 0; k < completedTask.length; k++) {
-//             completedTaskArr = completedTask[k];
-//             completedTaskArr.style.display = x;
-//         };
-
-//     }
-//     function unCompletedItem(y) {
-//         for (j = 0; j < unCompletedTask.length; j++) {
-//             unCompleteTaskArr = unCompletedTask[j];
-//             unCompleteTaskArr.style.display = y;
-//         };
-
-//     }
-//     if (filterChild == 1) {
-//         unCompletedItem("inline-block");
-//         completedItem("none");
-
-//     } else if (filterChild == 2) {
-//         unCompletedItem("none");
-//         completedItem("inline-block");
-
-//     } else {
-//         unCompletedItem("inline-block");
-//         completedItem("inline-block");
-//     }
-
-// }
-// deleteButton.onclick = function deleteTask() {
-//   TodoList.removeChild(Task);
-// };
-// editButton.onclick = function editTask() {
-//   if (Task.contentEditable == "true") {
-//     Task.contentEditable = false;
-//     editButton.innerHTML = "Edited";
-//   } else {
-//     var buttonGroup = document.getElementsByTagName("button");
-//     var i;
-//     Task.contentEditable = true;
-//     editButton.innerHTML = "Editing";
-//     for (i = 0; i < buttonGroup.length; i++) {
-//       buttonGroup[i].contentEditable = false;
-//     }
-//   }
-// };
-// completeButton.onclick = function completeTask() {
-//   var TaskStyle, completeBtnStyle;
-//   TaskStyle = Task.style;
-//   completeBtnStyle = completeButton.style;
-
-//   if (Task.style.backgroundColor == "green") {
-//     Task.style = "";
-//     completeButton.style = "";
-//     Task.classList.add("li-item");
-//     Task.classList.remove("completed-item");
-//     completeButton.innerHTML = "Complete";
-//   } else {
-//     TaskStyle.backgroundColor = "green";
-//     TaskStyle.color = "white";
-//     completeBtnStyle.backgroundColor = "grey";
-//     completeButton.innerHTML = "&#10004;";
-//     Task.classList.add("completed-item");
-//     Task.classList.remove("li-item");
-//   }
-// };
+  status = filterELChild;
+  renderPage();
+}
